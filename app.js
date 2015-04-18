04d2e7684f1367f7dfee6d38102c6289c5205b41
cmmisura14 <cmmisura14@gmail.com>
2015-04-18 13:40:06 -0500

    Third Commit

--- app.js
+++ app.js
@@ -24,8 +24,25 @@ console.log(playerCody);
 
 var dungeonArray = [];
 
-function hello(){
-	console.log(document.getElementById('name').value);
+function displayCreature(object){
+	var table = document.getElementById('statsTable');
+	var row = table.insertRow(table.rows.length);
+	var name = row.insertCell(0);
+	var strength = row.insertCell(1);
+	var dexterity = row.insertCell(2);
+	var constitution = row.insertCell(3);
+	var wisdom = row.insertCell(4);
+	var intelligence = row.insertCell(5);
+	var charisma = row.insertCell(6);
+	var playerClass = row.insertCell(7);
+	name.innerHTML = object.name;
+	strength.innerHTML = object.strength;
+	dexterity.innerHTML = object.dexterity;
+	constitution.innerHTML = object.constitution;
+	wisdom.innerHTML = object.wisdom;
+	intelligence.innerHTML = object.intelligence;
+	charisma.innerHTML = object.charisma;
+	playerClass.innerHTML = object.playerClass;
 
 }
 
@@ -39,6 +56,7 @@ function populateArray(){
 		}
 
 		dungeonArray.push(newCreature);
+		displayCreature(newCreature);
 }
 
 document.getElementById('addMonster').addEventListener('click', populateArray);
